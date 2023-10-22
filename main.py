05ef779069d4e259214a61e1b2abd6ee8911a721
Samyuktha Ganeshkumar <samyuktha19095@cse.ssn.edu.in>
2023-10-22 13:10:09 +0530

    create matches API

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from typing import Annotated
+import uvicorn
+
+from fastapi import Body, FastAPI
+import json
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
 
@@ -6,3 +11,55 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+def create_vector(sample):
+
+    with open('card_data.json') as json_file:
+
+        card_data = json.load(json_file)
+
+    everything = []
+
+    for category in card_data.keys():
+        everything.append(category)
+        everything += card_data[category]["skills"]
+        everything += card_data[category]["interests"]
+
+    indices = []
+
+    for element in sample:
+
+        indices.append(everything.index(element))
+
+    vector = [0] * len(everything)
+
+    for index in indices:
+
+        vector[index] = 1
+
+    return vector
+
+
+@app.post("/matches")
+def generate_matches(mentee: Annotated[list, Body(..., embed=True)]):
+
+    with open('random_mentors.json') as json_file:
+
+        random_mentors = json.load(json_file)
+
+    scores = []
+
+    for mentor in random_mentors:
+
+        vector = create_vector(random_mentors[mentor]["categories"] +
+                               random_mentors[mentor]["skills"] + random_mentors[mentor]["interests"])
+
+        score = cosine_similarity([create_vector(mentee)], [
+            vector])[0][0]
+
+        scores.append({"mentor": mentor, "score": score})
+
+    top5 = sorted(scores, key=lambda x: x["score"], reverse=True)[:3]
+
+    return {"matches": top5}
